d0184f2902f20154ee35688569df3012012be5d3
Hyori 효리 <hyorigo@icloud.com>
2023-05-26 20:19:15 +0800

    For calling funcs"

--- convert/call_test.go
+++ convert/call_test.go
@@ -1,8 +1,10 @@
 package convert_test
 
 import (
-	"go.starlark.net/starlark"
+	"fmt"
 	"testing"
+
+	"go.starlark.net/starlark"
 )
 
 func TestCallStarlarkFunctionInGo(t *testing.T) {
@@ -12,13 +14,42 @@ def greet(name="John"):
 
 greet_func = greet
 `
+	// run the starlark code
 	globals, err := execStarlark(code, nil)
 	if err != nil {
 		t.Fatalf(`expected no error, but got %v`, err)
 	}
+
+	// retrieve the starlark function
 	greet, ok := globals["greet_func"].(*starlark.Function)
 	if !ok {
 		t.Fatalf(`expected greet_func to be a *starlark.Function, but got %T`, globals["greet_func"])
 	}
-	t.Logf(`greet_func: %v`, greet)
+	thread := &starlark.Thread{
+		Name:  "test",
+		Print: func(_ *starlark.Thread, msg string) { fmt.Println("🌟", msg) },
+	}
+
+	// call the starlark function with no arguments
+	if res, err := starlark.Call(thread, greet, starlark.Tuple{}, nil); err != nil {
+		t.Fatalf(`expected no error while calling greet(), but got %v`, err)
+	} else if resStr, ok := res.(starlark.String); !ok {
+		t.Fatalf(`expected greet() to return a starlark.String, but got %T`, resStr)
+	} else if resStr.GoString() != `Hello, John!` {
+		t.Fatalf(`expected greet() to return "Hello, John!", but got %s`, resStr.GoString())
+	}
+
+	// call the starlark function with one argument
+	if res, err := starlark.Call(thread, greet, starlark.Tuple{starlark.String("Jane")}, nil); err != nil {
+		t.Fatalf(`expected no error while calling greet("Jane"), but got %v`, err)
+	} else if resStr, ok := res.(starlark.String); !ok {
+		t.Fatalf(`expected greet("Jane") to return a starlark.String, but got %T`, resStr)
+	} else if resStr.GoString() != `Hello, Jane!` {
+		t.Fatalf(`expected greet("Jane") to return "Hello, Jane!", but got %s`, resStr.GoString())
+	}
+
+	// call the starlark function with extra arguments
+	if _, err := starlark.Call(thread, greet, starlark.Tuple{starlark.String("Jane"), starlark.String("Doe")}, nil); err == nil {
+		t.Fatalf(`expected an error while calling greet("Jane", "Doe"), but got none`)
+	}
 }
