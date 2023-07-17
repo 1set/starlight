6f77f9bb9b042e5c47d822bb0d51bb842cd49720
Hyori 효리 <hyorigo@icloud.com>
2023-07-17 17:21:30 +0800

    Make Tuple

--- convert/conv.go
+++ convert/conv.go
@@ -168,8 +168,7 @@ func FromValue(v starlark.Value) interface{} {
 	}
 }
 
-// MakeStringDict makes a StringDict from the given arg. The types supported are
-// the same as ToValue.
+// MakeStringDict makes a StringDict from the given arg. The types supported are the same as ToValue.
 func MakeStringDict(m map[string]interface{}) (starlark.StringDict, error) {
 	dict := make(starlark.StringDict, len(m))
 	for k, v := range m {
@@ -182,14 +181,26 @@ func MakeStringDict(m map[string]interface{}) (starlark.StringDict, error) {
 	return dict, nil
 }
 
-// FromStringDict makes a map[string]interface{} from the given arg. Any
-// inconvertible values are ignored.
+// FromStringDict makes a map[string]interface{} from the given arg. Any inconvertible values are ignored.
 func FromStringDict(m starlark.StringDict) map[string]interface{} {
 	ret := make(map[string]interface{}, len(m))
 	for k, v := range m {
 		ret[k] = FromValue(v)
 	}
 	return ret
+}
+
+// MakeTuple makes a Starlark Tuple from the given Go slice. The types supported are the same as ToValue.
+func MakeTuple(v []interface{}) (starlark.Tuple, error) {
+	tuple := make(starlark.Tuple, len(v))
+	for i, val := range v {
+		item, err := ToValue(val)
+		if err != nil {
+			return nil, err
+		}
+		tuple[i] = item
+	}
+	return tuple, nil
 }
 
 // FromTuple converts a starlark.Tuple into a []interface{}.
